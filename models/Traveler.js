469ac8ae2c5148b41a3eb9eeb5f5bb8707170a8b
Karla Goo <karla_goo@yahoo.com>
2021-08-30 20:56:55 -0500

    added init files

---
+++ models/Traveler.js
@@ -0,0 +1,27 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Traveler extends Model { }
+
+Traveler.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true,
+            }
+        }
+    }
+);
---
+++ models/Trips.js
@@ -0,0 +1,34 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Trips extends Model { }
+
+Trip.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        trip_budget: {
+            type: DataTypes.FLOAT,
+            allowNull: false,
+        },
+        traveler_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'Traveler',
+                id: 'Traveler.id'
+            },
+
+        location_id: {
+            type: DataTypes.INTEGER,   
+            references: {
+                model: 'Location',
+                id: 'Location.id'
+            }
+        }
+    }
+    }
+);
